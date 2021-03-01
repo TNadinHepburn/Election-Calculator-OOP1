d833812c42ae7b7699a0809a6bbd50626971a940
ThomasNadinHepburn <tom.nadin.hepburn@gmail.com>
2021-03-01 10:31:05 +0000

    Added Input and output files.

--- Voting-Calculator/Program.cs
+++ Voting-Calculator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace Voting_Calculator
 {
@@ -10,6 +12,11 @@ namespace Voting_Calculator
     {
         static void Main(string[] args)
         {
+            int TotalSeats = 5;
+            var inputDataFilePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Assessment1InputData.txt");
+            var votesFile = File.ReadAllText(inputDataFilePath);
+            Console.WriteLine(votesFile);
+            Console.ReadKey();
         }
     }
 }
